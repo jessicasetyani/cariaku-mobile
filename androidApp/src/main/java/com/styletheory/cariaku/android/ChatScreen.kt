adcd46647cddbf807df38372339971821356c49b
Jessica Setyani <jessica@styletheory.co>
2024-09-11 16:11:02 +0700

    feat: Add TopAppBar to ChatScreen with ExperimentalMaterial3Api annotation

--- androidApp/src/main/java/com/styletheory/cariaku/android/ChatScreen.kt
+++ androidApp/src/main/java/com/styletheory/cariaku/android/ChatScreen.kt
@@ -20,11 +20,13 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -83,6 +85,7 @@ fun ChatScreen() {
  *
  * This function displays an icon and the assistant's name.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Header() {
     TopAppBar(
