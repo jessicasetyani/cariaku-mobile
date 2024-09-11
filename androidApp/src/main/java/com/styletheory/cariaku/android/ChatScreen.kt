a8e45bad70949c073efa7f1823bbab9dd08f029b
Jessica Setyani <jessica@styletheory.co>
2024-09-11 16:14:59 +0700

    style: Replace deprecated ArrowBack icon with ArrowBackIosNew icon in ChatScreen.kt

--- androidApp/src/main/java/com/styletheory/cariaku/android/ChatScreen.kt
+++ androidApp/src/main/java/com/styletheory/cariaku/android/ChatScreen.kt
@@ -5,7 +5,6 @@ import android.content.res.Resources
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,6 +18,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -107,7 +107,7 @@ fun Header(onNavigateBack: () -> Unit) {
         },
         navigationIcon = {
             IconButton(onClick = onNavigateBack) {
-                Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                Icon(Icons.Default.ArrowBackIosNew, contentDescription = "Back")
             }
         }
     )
