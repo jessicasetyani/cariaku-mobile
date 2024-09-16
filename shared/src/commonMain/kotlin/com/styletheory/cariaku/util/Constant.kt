40c69da869dc50094fc31a0fa16eb903f678de23
Jessica <jessica@styletheory.co>
2024-09-16 11:26:07 +0700

    change model robot AI

--- shared/src/commonMain/kotlin/com/styletheory/cariaku/util/Constant.kt
+++ shared/src/commonMain/kotlin/com/styletheory/cariaku/util/Constant.kt
@@ -4,7 +4,7 @@ package com.styletheory.cariaku.util
  * Created by Jessica Setyani on 15-09-2024.
  */
 object Constant {
-    val MODEL_AI_REFLECTION = "mattshumer/reflection-70b:free"
+    val MODEL_AI_REFLECTION = "nousresearch/hermes-3-llama-3.1-405b:free"
     val API_KEY_OPEN_ROUTE = "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"
     val PROJECT_NAME_CARI_AKU = "CariAku"
     val HEADER_X_TITLE = "X-Title"
