68c5fd6bd122342f3d56d9aef29280dc8f8a6108
jessica sw <jessica@styletheory.co>
2024-10-13 13:20:01 +0700

    feat: Add system bars padding and adjust bottom padding in WelcomePage

--- androidApp/src/main/java/com/styletheory/cariaku/android/ui/screen/WelcomePage.kt
+++ androidApp/src/main/java/com/styletheory/cariaku/android/ui/screen/WelcomePage.kt
@@ -6,13 +6,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -333,9 +333,10 @@ fun WelcomePageContent(
                 .padding(
                     start = 16.dp,
                     end = 16.dp,
-                    top = 86.dp,
-                    bottom = 16.dp
-                ) // Adjust this value based on the height of the SearchBar and BottomNavigationBar
+                    top = 86.dp
+                )
+                .systemBarsPadding()
+                .padding(bottom = 56.dp)
         ) {
             item {
                 Spacer(Modifier.height(16.dp))
@@ -362,6 +363,7 @@ fun WelcomePageContent(
         Box(
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(top = 16.dp)
                 .align(Alignment.BottomCenter)
         ) {
             BottomNavigationBar(
