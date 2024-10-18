6c4522a5e45f08780f052d683fc958f61b242e89
jessica.sw <jessica@styletheory.co>
2024-10-18 11:14:24 +0700

    add greetings to the HomeScreen

--- androidApp/src/main/java/com/styletheory/cariaku/android/ui/screen/home/HomeScreen.kt
+++ androidApp/src/main/java/com/styletheory/cariaku/android/ui/screen/home/HomeScreen.kt
@@ -1,159 +1,123 @@
 package com.styletheory.cariaku.android.ui.screen.home
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.styletheory.cariaku.android.R
-import com.styletheory.cariaku.android.ui.screen.Feature
-import com.styletheory.cariaku.android.ui.screen.home.model.AssistantMenuContent
-import com.styletheory.cariaku.android.ui.screen.home.model.BottomMenu
-import com.styletheory.cariaku.android.ui.screen.home.model.BottomMenuContent
-import com.styletheory.cariaku.android.ui.theme.Beige1
-import com.styletheory.cariaku.android.ui.theme.Beige2
-import com.styletheory.cariaku.android.ui.theme.Beige3
-import com.styletheory.cariaku.android.ui.theme.BlueViolet1
-import com.styletheory.cariaku.android.ui.theme.BlueViolet2
-import com.styletheory.cariaku.android.ui.theme.BlueViolet3
-import com.styletheory.cariaku.android.ui.theme.ButtonBlue
-import com.styletheory.cariaku.android.ui.theme.DeepBlue
-import com.styletheory.cariaku.android.ui.theme.LightGreen1
-import com.styletheory.cariaku.android.ui.theme.LightGreen2
-import com.styletheory.cariaku.android.ui.theme.LightGreen3
-import com.styletheory.cariaku.android.ui.theme.LightRed
-import com.styletheory.cariaku.android.ui.theme.OrangeYellow1
-import com.styletheory.cariaku.android.ui.theme.OrangeYellow2
-import com.styletheory.cariaku.android.ui.theme.OrangeYellow3
-import com.styletheory.cariaku.android.ui.theme.TextWhite
-import com.styletheory.cariaku.android.util.standardQuadFromTo
 import java.time.LocalTime
 
+import androidx.compose.ui.graphics.Color
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun HomeScreen(onOpenChat: () -> Unit) {
-    Box(
-        modifier = Modifier
-            .background(DeepBlue)
-            .fillMaxSize()
-    ) {
-        Column {
-            GreetingSection()
-            CariAkuAndalanSection(
-                chips = listOf(
-                    AssistantMenuContent("Assistant 1", R.drawable.ic_placeholder_assistant),
-                    AssistantMenuContent("Assistant 2", R.drawable.ic_placeholder_assistant),
-                    AssistantMenuContent("Assistant 3", R.drawable.ic_placeholder_assistant),
-                    AssistantMenuContent("Assistant 4", R.drawable.ic_placeholder_assistant)
-                )
-            )
-            CurrentMeditation()
-            FeatureSection(
-                features = listOf(
-                    Feature(
-                        title = "Sleep meditation",
-                        iconId = R.drawable.ic_videocam,
-                        BlueViolet1,
-                        BlueViolet2,
-                        BlueViolet3
-                    ),
-                    Feature(
-                        title = "Tips for sleeping",
-                        R.drawable.ic_videocam,
-                        LightGreen1,
-                        LightGreen2,
-                        LightGreen3
-                    ),
-                    Feature(
-                        title = "Night island",
-                        R.drawable.ic_headphone,
-                        OrangeYellow1,
-                        OrangeYellow2,
-                        OrangeYellow3
-                    ),
-                    Feature(
-                        title = "Calming sounds",
-                        R.drawable.ic_headphone,
-                        Beige1,
-                        Beige2,
-                        Beige3
-                    )
-                )
-            )
+fun HomeScreen(
+    onOpenChat: () -> Unit
+) {
+
+    Scaffold(
+        topBar = {
+            CustomTopAppBar()
         }
-        BottomMenu(
-            items = listOf(
-                BottomMenuContent("Home", Icons.Default.Home),
-                BottomMenuContent("Favorite", Icons.Default.Favorite),
-                BottomMenuContent("Profile", Icons.Default.Person),
-            ), modifier = Modifier.align(Alignment.BottomCenter)
+    ) { innerPadding ->
+        ContentArea(modifier = Modifier.padding(innerPadding), username = "Jessica")
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTopAppBar() {
+    TopAppBar(
+        title = {
+            Text(text = "CariAku")
+        }, navigationIcon = {
+            IconButton(onClick = { /* Handle icon click */ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_logo),
+                    contentDescription = "Menu Icon",
+                    tint = Color.Unspecified
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                onClick = {
+
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search icon"
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimary
         )
+    )
+}
+
+@Composable
+fun ContentArea(modifier: Modifier = Modifier, username: String) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        GreetingSection(username = username)
     }
 }
 
-
 @Composable
 fun GreetingSection(
-    name: String = "Jessica"
+    username: String
 ) {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(15.dp)
     ) {
         Column(
             verticalArrangement = Arrangement.Center
         ) {
             Text(
-                text = "${getGreeting()}, $name!",
-                style = MaterialTheme.typography.headlineMedium
+                text = "${getGreeting()}, $username!",
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Center
             )
             Text(
                 text = "Masih semangat nih? CariAku siap bantu kamu 24/7 loh!",
-                style = MaterialTheme.typography.bodySmall
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.padding(top = 4.dp)
             )
         }
-        Icon(
-            Icons.Default.Search,
-            contentDescription = "Search",
-            tint = Color.White,
-            modifier = Modifier.size(24.dp)
-        )
     }
 }
 
@@ -163,165 +127,5 @@ fun getGreeting(time: LocalTime = LocalTime.now()): String {
         in 12..16 -> "Good Afternoon"
         in 17..20 -> "Good Evening"
         else -> "Good Night"
-    }
-}
-
-@Composable
-fun CurrentMeditation(
-    color: Color = LightRed
-) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier
-            .padding(15.dp)
-            .clip(RoundedCornerShape(10.dp))
-            .background(color)
-            .padding(horizontal = 15.dp, vertical = 20.dp)
-            .fillMaxWidth()
-    ) {
-        Column {
-            Text(
-                text = "Daily Thought",
-                style = MaterialTheme.typography.headlineMedium
-            )
-            Text(
-                text = "Meditation • 3-10 min",
-                style = MaterialTheme.typography.bodySmall,
-                color = TextWhite
-            )
-        }
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .background(ButtonBlue)
-                .padding(10.dp)
-        ) {
-            Icon(
-                Icons.Default.PlayArrow,
-                contentDescription = "Play",
-                tint = Color.White,
-                modifier = Modifier.size(16.dp)
-            )
-        }
-    }
-}
-
-@Composable
-fun FeatureSection(features: List<Feature>) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Text(
-            text = "Features",
-            style = MaterialTheme.typography.headlineLarge,
-            modifier = Modifier.padding(15.dp)
-        )
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
-            modifier = Modifier.fillMaxHeight()
-        ) {
-            items(features.size) {
-                FeatureItem(feature = features[it])
-            }
-        }
-    }
-}
-
-@Composable
-fun FeatureItem(
-    feature: Feature
-) {
-    BoxWithConstraints(
-        modifier = Modifier
-            .padding(7.5.dp)
-            .aspectRatio(1f)
-            .clip(RoundedCornerShape(10.dp))
-            .background(feature.darkColor)
-    ) {
-        val width = constraints.maxWidth
-        val height = constraints.maxHeight
-
-        // Medium colored path
-        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
-        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
-        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
-        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
-        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
-
-        val mediumColoredPath = Path().apply {
-            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
-            standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
-            standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
-            standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
-            standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
-            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
-            lineTo(-100f, height.toFloat() + 100f)
-            close()
-        }
-
-        // Light colored path
-        val lightPoint1 = Offset(0f, height * 0.35f)
-        val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
-        val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
-        val lightPoint4 = Offset(width * 0.65f, height.toFloat())
-        val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
-
-        val lightColoredPath = Path().apply {
-            moveTo(lightPoint1.x, lightPoint1.y)
-            standardQuadFromTo(lightPoint1, lightPoint2)
-            standardQuadFromTo(lightPoint2, lightPoint3)
-            standardQuadFromTo(lightPoint3, lightPoint4)
-            standardQuadFromTo(lightPoint4, lightPoint5)
-            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
-            lineTo(-100f, height.toFloat() + 100f)
-            close()
-        }
-        Canvas(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            drawPath(
-                path = mediumColoredPath,
-                color = feature.mediumColor
-            )
-            drawPath(
-                path = lightColoredPath,
-                color = feature.lightColor
-            )
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(15.dp)
-        ) {
-            Text(
-                text = feature.title,
-                style = MaterialTheme.typography.headlineMedium,
-                lineHeight = 26.sp,
-                modifier = Modifier.align(Alignment.TopStart)
-            )
-            Icon(
-                painter = painterResource(id = feature.iconId),
-                contentDescription = feature.title,
-                tint = Color.White,
-                modifier = Modifier.align(Alignment.BottomStart)
-            )
-            Text(
-                text = "Start",
-                color = TextWhite,
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier
-                    .clickable {
-                        // Handle the click
-                    }
-                    .align(Alignment.BottomEnd)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(ButtonBlue)
-                    .padding(vertical = 6.dp, horizontal = 15.dp)
-            )
-        }
     }
 }
